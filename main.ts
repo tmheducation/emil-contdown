1ef9a1bdf9e09c907efe6a46cc85fc31b53a757b
tmheducation <68049755+tmheducation@users.noreply.github.com>
2022-10-10 12:48:18 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showNumber(0)
+    basic.showNumber(0)
+    basic.showNumber(0)
+    basic.showNumber(0)
+    basic.showNumber(0)
+    basic.showNumber(0)
+    basic.showNumber(0)
+    basic.showNumber(0)
+    basic.showNumber(0)
+})
